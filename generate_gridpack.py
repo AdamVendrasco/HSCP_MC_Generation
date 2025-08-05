e850578b6fd66659e04e374dd02c9076da5b74b9
Adam Vendrasco <avendras@vols.utk.edu>
2025-08-05 16:08:05 -0500

    updated generation_gridpack script

--- generate_gridpack.py
+++ generate_gridpack.py
@@ -1,79 +1,63 @@
 #!/usr/bin/env python
 import os
 import sys
-import pathlib
+from pathlib import Path
 import argparse
 import time
 import getpass
 
-'''
-example command
-python3 generate_gridpack.py --tag Run2023 -o <path_to_output dir> -i <path_to_cards> -q nextweek -j 8
-'''
-nobackup='/uscms_data/d1/{username}'.format(username=getpass.getuser())
+nobackup = f'/uscms_data/d1/{getpass.getuser()}'
 
-tagname='Run2023'
+tagname = 'Run2023'
 
-goutput='{basedir}/gridpack/wplustest'.format(basedir=nobackup)
-cinput ='{basedir}/mg5work/mycards/wplustest'.format(basedir=nobackup)
+goutput = f'{nobackup}/gridpack/wplustest'
+cinput = f'{nobackup}/mg5work/mycards/wplustest'
 
-tarball_prefix='_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz'
-tarball_prefix='_tarball.tar.xz'
+EXEC_NAME = "batchexec"
+LOGS_NAME = "batchlogs"
 
-EXEC_NAME="batchexec"
-LOGS_NAME="batchlogs"
-RUNS_NAME="batchruns"
-
-WORK_DIR=os.getcwd()
+WORK_DIR = os.getcwd()
 
 class ArgumentParser(argparse.ArgumentParser):
-  def __init__(self):
-    argparse.ArgumentParser.__init__(self, usage="%(prog)s <action> <command file> [options]")
-    self.add_argument("--tag"         , dest="tag"       , type=str           , default=tagname , help="An identifier of the run, please take into account that the code will rewrite files by default, so change tag accordingly")
-    self.add_argument("-f","--force"  , dest="force"     , action="store_true", default=False , help="Force temp folder recreation and just overall ignore warnings.")
-    # self.add_argument("-o","--out"    , dest="out"       , type=str           , default=os.getcwd(), help="Output folder for gridpacks, default is running one which probably is a bad idea")
-    self.add_argument("-o","--out"    , dest="out"       , type=str           , default=goutput, help="Output folder for gridpacks, default is running one which probably is a bad idea")
-    # self.add_argument("-i","--in"     , dest="inF"       , type=str           , default=None, help="Input folder with the cards")
-    self.add_argument("-i","--in"     , dest="inF"       , type=str           , default=cinput, help="Input folder with the cards")
-    self.add_argument("-q","--queue"  , dest="queue"     , type=str           , default="tomorrow", help="Condor queue to be used. Default is tomorrow (1 day). Other logical options are testmatch (3 days), nextweek (1 week), workday (8 hours)" )
-    self.add_argument("-j","--jobs"   , dest="jobs"      , type=int           , default="4", help="Request this number of cores per job" )
-    self.add_argument("-n","--new"    , dest="new"       , action="store_true", default=False , help="If activated, will check if gridpack already exists in destination and skip submission if it does")
-    self.add_argument("-p","--pretend", dest="pretend"   , action="store_true", default=False , help="Only create folders, don't run anything")
-    self.add_argument("-l","--local",   dest="local"     , action="store_true", default=False , help="Only create folders, don't run anything")
-
+    def __init__(self):
+        super().__init__(usage="%(prog)s [options]")
+        self.add_argument("--tag", type=str, default=tagname)
+        self.add_argument("-f", "--force", action="store_true")
+        self.add_argument("-o", "--out", type=str, default=goutput)
+        self.add_argument("-i", "--in", dest="inF", type=str, default=cinput)
+        self.add_argument("-q", "--queue", type=str, default="tomorrow")
+        self.add_argument("-j", "--jobs", type=int, default=4)
+        self.add_argument("-p", "--pretend", action="store_true")
 
 def create_submit_file(process, cards, tag, jobs, queue):
-  submit_file = 'submit.sub'
-  with open(submit_file,'w') as fi:
-    fi.write("""\
+    submit_file = 'submit.sub'
+    with open(submit_file, 'w') as fi:
+        fi.write(f"""\
 executable              = $(filename)
 arguments               = $(ClusterId)$(ProcId)
-transfer_input_files    = {CARDS}
-output                  = batchlogs_{TAG}/{PROCESS}_$(ClusterId).$(ProcId).out
-error                   = batchlogs_{TAG}/{PROCESS}_$(ClusterId).$(ProcId).err
-log                     = batchlogs_{TAG}/{PROCESS}_$(ClusterId).log
-RequestCPUs             = {JOBS}
-+JobFlavour             = "{QUEUE}"
-## +MaxRuntime          = 2000000
+transfer_input_files    = {cards}
+output                  = batchlogs_{tag}/{process}_$(ClusterId).$(ProcId).out
+error                   = batchlogs_{tag}/{process}_$(ClusterId).$(ProcId).err
+log                     = batchlogs_{tag}/{process}_$(ClusterId).log
+RequestCPUs             = {jobs}
++JobFlavour             = \"{queue}\"
+request_memory          = 8000
 
-queue filename matching (batchexec_{TAG}/job_{PROCESS}.sh)""".format(PROCESS=process, CARDS=cards, TAG=tag, JOBS=jobs, QUEUE=queue))
-    fi.close()
+queue filename matching (batchexec_{tag}/job_{process}.sh)
+""")
 
+def create_exec_file(process, cardsdir, exec_dir):
+    exec_file = f'{exec_dir}/job_{process}.sh'
 
-def create_exec_file(process, cardsdir, outdir, exec_dir, logs_dir, local):
-  exec_file = '{}/job_{}.sh'.format(exec_dir, process)
-
-  LocalScript="true" if local else "false"
-
-  with open(exec_file,'w') as fi:
-    fi.write("""#!/bin/sh
+    with open(exec_file, 'w') as fi:
+        fi.write(f"""#!/bin/sh
 echo ""
 echo `date` at `hostname`
 echo ""
 workarea=$PWD
-LOCAL="{LOCAL}"
-# First we do a sparse checkout of genproductions to avoid copying the whole thing and to also have the latests set of patches available
-# We might want to fix a commit sha here to ensure it doesn't pull some tricks
+PROCESS="{process}"
+LOCAL="false"
+
 git clone https://github.com/cms-sw/genproductions.git --no-checkout genproductions --depth 1
 cd genproductions
 git config core.sparsecheckout true
@@ -83,110 +67,58 @@ echo bin/MadGraph5_aMCatNLO >> .git/info/sparse-checkout
 git read-tree -m -u HEAD
 cd bin/MadGraph5_aMCatNLO/
 
-echo ""
-echo "Entering '$PWD'"
-echo ""
+mkdir cards/${{PROCESS}}
+cp $workarea/${{PROCESS}}_*.dat cards/${{PROCESS}}/
 
-# Copy input cards
-#echo "Copy input cards from: {CARDSDIR}"
-#ls {CARDSDIR}
+echo "./cards/${{PROCESS}}/:"
+ls cards/${{PROCESS}}/
 
-#cp -r {CARDSDIR} ./cards/
-#xrdcp -r {CARDSDIR} ./cards/
-
-# Create cards
-mkdir cards/{PROCESS}
-if $LOCAL; then
-  cp {CARDSDIR}/{PROCESS}*.dat cards/{PROCESS}/
+if [ ! -d cards/${{PROCESS}} ]; then 
+  echo "ERROR: cards/${{PROCESS}} does NOT exist"
 else
-  cp $workarea/{PROCESS}*.dat cards/{PROCESS}/
-fi
-echo "./cards/{PROCESS}/:"
-ls cards/{PROCESS}/
-
-
-echo ""
-if [ ! -d cards/{PROCESS} ]; then echo "ERROR: cards/{PROCESS} does NOT exist"
-else
-  #Run the script
-  sh gridpack_generation.sh {PROCESS} cards/{PROCESS}
-  #Copy the gridpack back to somewhere readable
-  mv {PROCESS}*.tar.xz {OUTDIR}/
-  mv {PROCESS}.log {WORKDIR}/{LOGSDIR}/
+  sh gridpack_generation.sh ${{PROCESS}} cards/${{PROCESS}}
+  mv ${{PROCESS}}*.tar.xz $workarea/
+  mv ${{PROCESS}}.log $workarea/
 fi
 
 sleep 3
-#Do some cleanup just to be tidy
-cd ../../../
-ls
-rm -rf genproductions""".format(CARDSDIR=cardsdir, PROCESS=process, OUTDIR=outdir, LOGSDIR=logs_dir, WORKDIR=WORK_DIR, LOCAL=LocalScript))
-
-  return exec_file
-
-def get_cards(path, absolute_path=False):
-  files = os.listdir('{}'.format(path))
-  if absolute_path:
-    files = [f'{path}/{f}' for f in files]
-  return ','.join(files)
+""")
+    os.system(f"chmod +x {exec_file}")
+    return exec_file
 
 
-def create_dirs(TAG, LOCAL):
-  exec_dir='{}_{}'.format(EXEC_NAME, TAG)
-  logs_dir='{}_{}'.format(LOGS_NAME, TAG)
-  runs_dir='{}_{}'.format(RUNS_NAME, TAG)
+def get_cards(path, process):
+    files = [f'{path}/{process}_proc_card.dat',
+             f'{path}/{process}_run_card.dat',
+             f'{path}/{process}_param_card.dat']
+    return ','.join(files)
 
-  if not (os.path.isdir(exec_dir) or os.path.isdir(logs_dir)):
-    print('Creating directories ...')
-    os.system("mkdir {}".format(exec_dir))
-    os.system("mkdir {}".format(logs_dir))
-    print('Done!')
-  if (not os.path.isdir(runs_dir)) and LOCAL:
-    print('Creating Run directory ...')
-    os.system("mkdir {}".format(runs_dir))
-    print('Done!')
+def create_dirs(tag):
+    exec_dir = f'{EXEC_NAME}_{tag}'
+    logs_dir = f'{LOGS_NAME}_{tag}'
+    os.makedirs(exec_dir, exist_ok=True)
+    os.makedirs(logs_dir, exist_ok=True)
+    return exec_dir, logs_dir
 
-  return exec_dir,logs_dir,runs_dir
-
-##################################################################################################
-##################################################################################################
 if __name__ == "__main__":
-  args = ArgumentParser().parse_args()
+    args = ArgumentParser().parse_args()
 
-  process_name=args.inF.split('/')[-1]
+    process_name = Path(args.inF).parent.name
+    print('Process:', process_name)
 
-  print('Process:',process_name)
+    exec_dir, logs_dir = create_dirs(args.tag)
 
-  # exit()
+    if "/afs/" in args.out and not args.force:
+        raise RuntimeError("Sending gridpacks to afs. Run with -f to force.")
+    os.makedirs(args.out, exist_ok=True)
 
-  exec_dir,logs_dir,runs_dir = create_dirs(args.tag, args.local)
+    EXECFILE = create_exec_file(process_name, args.inF, exec_dir)
 
-  if "/afs/" in args.out and not(args.force):
-    raise RuntimeError("Error: you are sending your final gridpacks to be copied to afs, this might be a bad idea unles you have dedicated space for them. Run with force mode (-f) to force this.")
-  elif not(os.path.isdir(args.out)) and not ('root://' in args.out):
-    os.system("mkdir {}".format(args.out))
+    cards = get_cards(args.inF, process_name)
 
-  ##### creating and sending jobs #####
-
-  EXECFILE=create_exec_file(process_name, args.inF, args.out, exec_dir, logs_dir, args.local)
-
-  os.system("chmod 755 {}".format(EXECFILE))
-
-  cards = get_cards(args.inF, True)
-
-  if args.local:
-    os.system('cp {} {}/'.format(EXECFILE, runs_dir))
-  else:
     create_submit_file(process_name, cards, args.tag, args.jobs, args.queue)
 
-    ###### sends bjobs ######
     if not args.pretend:
-      os.system("echo submit.sub")
-      time.sleep(3)
-      os.system("condor_submit submit.sub")
-      
-      print()
-      print("Your jobs are here:")
-      os.system("condor_q")
-      print()
-      print('END')
-      print()
+        time.sleep(2)
+        os.system("condor_submit submit.sub")
+        os.system("condor_q")
