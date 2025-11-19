fbacbda2a2ec4c29a9822b6e8b9e26f6888830b8
Adam Vendrasco <avendras@vols.utk.edu>
2025-11-19 11:37:34 -0600

    Remove obsolete files

--- run_directories/ms-Rhadron_mstop_2000/output-configs/run_ms-Rhadron_mstop_2000__qcut100.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd /uscms/home/avendras/nobackup/HSCP
-export FRAGMENT_NAME="ms-Rhadron_mstop_2000-fragment_Jet_matching_ON__qcut100.py"
-./run-gridpack_GEN-SIM.sh ms-Rhadron_mstop_2000 CMSSW_10_6_47 100 qcut100
