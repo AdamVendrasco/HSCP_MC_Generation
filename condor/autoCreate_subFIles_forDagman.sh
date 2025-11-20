7d93c6f0f0d382a2af70f10737de1a775b61a806
Adam Vendrasco <avendras@vols.utk.edu>
2025-11-20 10:35:22 -0600

    update

--- condor/autoCreate_subFIles_forDagman.sh
+++ condor/autoCreate_subFIles_forDagman.sh
@@ -3,11 +3,11 @@ PYTHON=python3
 GP_SCRIPT="condor_generate_gridpacks.py"
 
 OUT_DIR="/eos/uscms/store/user/avendras/HSCP/gridpack_output/tarballs/RunIII"
-MASS=1600
+MASS=700
 TAG="HSCPstop_M-${MASS}"
 QUEUE="tomorrow"
 JOBS=2
-XQCUTS="1"
+XQCUTS="10 20 40 60 80 100 110 120 130 140 150 160 170"
 
 for XQCUT in ${XQCUTS}; do
   IN_DIR="/uscms/home/avendras/nobackup/HSCP/run_directories/Run-3/stop-based/HSCPstop_M-${MASS}_xqcut${XQCUT}/"
