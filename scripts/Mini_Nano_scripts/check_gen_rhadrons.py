3c9b47d160d544824ce7273aad068c8cfde45dd0
Adam Vendrasco <avendras@vols.utk.edu>
2026-02-02 15:34:43 -0600

    updated binning on histograms/included edmDump of miniAod

--- scripts/Mini_Nano_scripts/check_gen_rhadrons.py
+++ scripts/Mini_Nano_scripts/check_gen_rhadrons.py
@@ -13,25 +13,39 @@ ROOT.AutoLibraryLoader.enable()
 FILES = [
     "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.0-MINIAODSIM.root",
     "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.1-MINIAODSIM.root",
-    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.2-MINIAODSIM.root"
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.2-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.3-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.4-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.5-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.6-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.7-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.8-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.9-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.10-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.11-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.12-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.13-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.14-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.15-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.16-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.17-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.18-MINIAODSIM.root",
+    "root://cmseos.fnal.gov//eos/uscms/store/user/avendras/HSCP/gridpack_output/root_files/PrivateSamples-Gael/Pileup_included/HSCP-Gluino_Par-M-1800/HSCP-Gluino_Par-M-1800_xqcut150_el8_amd64_gcc12_2024-CMSSW_14_0_22-n5000-Run3_2024-job82856820.19-MINIAODSIM.root"
+    
 ]
 
 THRESH = 1_000_000
-MAX_EVENTS_TO_PRINT = 5
+MAX_EVENTS_TO_PRINT = 1
 
 NBINS = 40
-PT_MIN, PT_MAX   = 0.0, 2000.0
-ETA_MIN, ETA_MAX = -5.0, 5.0
+PT_MIN, PT_MAX   = 0.0, 3500.0
+ETA_MIN, ETA_MAX = -6.0, 6.0
 PHI_MIN, PHI_MAX = -4.0, 4.0
 MASS_MIN, MASS_MAX = 1000.0, 2600.0
 
 counts_packed = Counter()
 counts_pruned = Counter()
 
-# -------------------------
-# FWLite Handles (IMPORTANT)
-# Handle() takes ONLY a C++ type string
-# -------------------------
 h_packed = Handle("std::vector<pat::PackedGenParticle>")
 h_pruned = Handle("std::vector<reco::GenParticle>")
 h_GenMET = Handle("std::vector<reco::GenMET>")
@@ -40,9 +54,6 @@ print("\n[Setup] Looking at std::vector<pat::PackedGenParticle> (packedGenPartic
 print("[Setup] Looking at std::vector<reco::GenParticle> (prunedGenParticles)")
 print("[Setup] Looking at std::vector<reco::GenMET> (genMetTrue)")
 
-# -------------------------
-# Histograms: R-hadrons
-# -------------------------
 h_lead_pt    = ROOT.TH1D("lead_rhad_pt_packed",     ";leading packed R-hadron p_{T} [GeV];Events", NBINS, PT_MIN, PT_MAX)
 h_sublead_pt = ROOT.TH1D("sublead_rhad_pt_packed",  ";subleading packed R-hadron p_{T} [GeV];Events", NBINS, PT_MIN, PT_MAX)
 
@@ -55,15 +66,13 @@ h_sublead_phi= ROOT.TH1D("sublead_rhad_phi_packed", ";subleading packed R-hadron
 h_lead_mass  = ROOT.TH1D("lead_rhad_mass_packed",   ";leading packed R-hadron mass [GeV];Events", NBINS, MASS_MIN, MASS_MAX)
 h_sublead_mass = ROOT.TH1D("sublead_rhad_mass_packed",";subleading packed R-hadron mass [GeV];Events", NBINS, MASS_MIN, MASS_MAX)
 
-h_lead_sublead_Delta_phi = ROOT.TH1D( "lead_sublead_Delta_phi_packed", ";|#Delta#phi(lead, sublead)|;Events", NBINS, 0.0, 3.7)
+h_lead_sublead_Delta_phi = ROOT.TH1D("lead_sublead_Delta_phi_packed", ";|#Delta#phi(lead, sublead)|;Events", NBINS, 0.0, 3.7)
 
-# GEN MET plots (more realistic binning)
-h_GenMET_pt    = ROOT.TH1D("GenMET_pt",    ";GEN MET p_{T} [GeV];Events", 100, 0.0, 500.0)
-h_GenMET_phi   = ROOT.TH1D("GenMET_phi",   ";GEN MET #phi;Events",        64, -3.5, 3.5)
-h_GenMET_px    = ROOT.TH1D("GenMET_px",    ";GEN MET p_{x} [GeV];Events", 120, -600.0, 600.0)
-h_GenMET_py    = ROOT.TH1D("GenMET_py",    ";GEN MET p_{y} [GeV];Events", 120, -600.0, 600.0)
-h_GenMET_sumEt = ROOT.TH1D("GenMET_sumEt", ";GEN MET #SigmaE_{T} [GeV];Events", 200, 0.0, 5000.0)
-
+h_GenMET_pt    = ROOT.TH1D("GenMET_pt",    ";GEN MET p_{T} [GeV];Events", 200, 0.0, 1000.0)
+h_GenMET_phi   = ROOT.TH1D("GenMET_phi",   ";GEN MET #phi;Events",        70, -3.5, 3.5)
+h_GenMET_px    = ROOT.TH1D("GenMET_px",    ";GEN MET p_{x} [GeV];Events", 200, -1000.0, 1000.0)
+h_GenMET_py    = ROOT.TH1D("GenMET_py",    ";GEN MET p_{y} [GeV];Events", 200, -1000.0, 1000.0)
+h_GenMET_sumEt = ROOT.TH1D("GenMET_sumEt", ";GEN MET #SigmaE_{T} [GeV];Events", 200, 0.0, 9000.0)
 
 for h in (
     h_lead_pt, h_sublead_pt,
@@ -92,21 +101,17 @@ for FILE in FILES:
         packed = h_packed.product()
         pruned = h_pruned.product()
 
-        
         got_genmet = evt.getByLabel("genMetTrue", h_GenMET)
         if not got_genmet:
             got_genmet = evt.getByLabel(("genMetTrue", "", "DIGI2RAW"), h_GenMET)
 
         if got_genmet:
-            genMetVec = h_GenMET.product()
-            # usually size==1, but loop safely
-            for met in genMetVec:
+            for met in h_GenMET.product():
                 h_GenMET_pt.Fill(float(met.pt()))
                 h_GenMET_phi.Fill(float(met.phi()))
                 h_GenMET_px.Fill(float(met.px()))
                 h_GenMET_py.Fill(float(met.py()))
                 h_GenMET_sumEt.Fill(float(met.sumEt()))
-
 
         for particle in packed:
             pdgid = int(particle.pdgId())
@@ -156,7 +161,6 @@ for FILE in FILES:
             dphi = float(tlv1.DeltaPhi(tlv2))
             h_lead_sublead_Delta_phi.Fill(abs(dphi))
 
-    
         if MAX_EVENTS_TO_PRINT != 0 and (MAX_EVENTS_TO_PRINT < 0 or printed_this_file < MAX_EVENTS_TO_PRINT):
             printed_this_file += 1
 
@@ -205,9 +209,13 @@ CMS.SetExtraText("Private Work: Gluino GEN-level MC Simulation")
 CMS.SetLumi(None, run="Run 3")
 CMS.SetEnergy(13.6)
 
-def draw_single(name, xmin, xmax, h, xtitle, ytitle, outpng, yoffset=1.45, titlesize=0.045, labelsize=0.040):
+def draw_single(name, xmin, xmax, h, xtitle, ytitle, outpng, yoffset=1.45, titlesize=0.045, labelsize=0.040, logy=False, ymin_log=0.1):
     ymax = max(h.GetMaximum(), 1.0) * 1.35
-    canv = CMS.cmsCanvas(name, xmin, xmax, 0.0, ymax, xtitle, ytitle, square=False, iPos=11)
+    ymin = ymin_log if logy else 0.0
+    canv = CMS.cmsCanvas(name, xmin, xmax, ymin, ymax, xtitle, ytitle, square=False, iPos=11)
+    if logy:
+        canv.SetLogy(True)
+        h.SetMinimum(ymin_log)
 
     frame = CMS.GetCmsCanvasHist(canv)
     frame.GetXaxis().SetTitleSize(titlesize)
@@ -224,9 +232,16 @@ def draw_single(name, xmin, xmax, h, xtitle, ytitle, outpng, yoffset=1.45, title
     canv.SaveAs(outpng)
     print(f"[Output] wrote PNG: {outpng}")
 
-def draw_overlay(name, xmin, xmax, h1, h2, xtitle, ytitle, outpng, yoffset=1.45, titlesize=0.045, labelsize=0.040):
+
+def draw_overlay(name, xmin, xmax, h1, h2, xtitle, ytitle, outpng, yoffset=1.45, titlesize=0.045, labelsize=0.040, logy=False, ymin_log=0.1):
     ymax = max(h1.GetMaximum(), h2.GetMaximum(), 1.0) * 1.35
-    canv = CMS.cmsCanvas(name, xmin, xmax, 0.0, ymax, xtitle, ytitle, square=False, iPos=11)
+    ymin = ymin_log if logy else 0.0
+
+    canv = CMS.cmsCanvas(name, xmin, xmax, ymin, ymax, xtitle, ytitle, square=False, iPos=11)
+    if logy:
+        canv.SetLogy(True)
+        h1.SetMinimum(ymin_log)
+        h2.SetMinimum(ymin_log)
 
     frame = CMS.GetCmsCanvasHist(canv)
     frame.GetXaxis().SetTitleSize(titlesize)
@@ -250,15 +265,16 @@ def draw_overlay(name, xmin, xmax, h1, h2, xtitle, ytitle, outpng, yoffset=1.45,
     print(f"[Output] wrote PNG: {outpng}")
 
 # R-hadron plots
-draw_overlay("c_rhad_pt_packed",   PT_MIN,   PT_MAX,   h_lead_pt,   h_sublead_pt,   "packed R-hadron p_{T} [GeV]", "Events", "rhad_pt_packed.png",   yoffset=1.01)
+draw_overlay("c_rhad_pt_packed",   PT_MIN,   PT_MAX,   h_lead_pt,   h_sublead_pt,   "packed R-hadron p_{T} [GeV]", "Events", "rhad_pt_packed.png",    yoffset=1.01)
 draw_overlay("c_rhad_eta_packed",  ETA_MIN,  ETA_MAX,  h_lead_eta,  h_sublead_eta,  "packed R-hadron #eta",       "Events", "rhad_eta_packed.png",  yoffset=1.01)
-draw_overlay("c_rhad_phi_packed",  PHI_MIN,  PHI_MAX,  h_lead_phi,  h_sublead_phi,  "packed R-hadron #phi",       "Events", "rhad_phi_packed.png",  yoffset=1.01)
-draw_overlay("c_rhad_mass_packed", MASS_MIN, MASS_MAX, h_lead_mass, h_sublead_mass, "packed R-hadron mass [GeV]",  "Events", "rhad_mass_packed.png", yoffset=1.01)
-draw_single ("c_dphi",             0.0,      5.0,      h_lead_sublead_Delta_phi,   "|#Delta#phi(lead, sublead)|", "Events", "lead_sublead_dphi_packed.png", yoffset=1.01)
+draw_overlay("c_rhad_phi_packed",  PHI_MIN,  PHI_MAX,  h_lead_phi,  h_sublead_phi,  "packed R-hadron #phi",       "Events", "rhad_phi_packed.png",   yoffset=1.01)
+draw_overlay("c_rhad_mass_packed", MASS_MIN, MASS_MAX, h_lead_mass, h_sublead_mass, "packed R-hadron mass [GeV]",  "Events", "rhad_mass_packed.png",  yoffset=1.01)
+draw_single ("c_dphi",             0.0,      3.5,      h_lead_sublead_Delta_phi,   "|#Delta#phi(lead, sublead)|", "Events", "lead_sublead_dphi_packed.png",yoffset=1.01)
 
 # GEN MET plots
-draw_single("c_genmet_pt",     0.0,   500.0,  h_GenMET_pt,    "GEN MET p_{T} [GeV]",        "Events", "genmet_pt.png",     yoffset=1.01)
-draw_single("c_genmet_phi",   -3.5,   3.5,     h_GenMET_phi,   "GEN MET #phi",               "Events", "genmet_phi.png",    yoffset=1.01)
-draw_single("c_genmet_px",   -600.0,  600.0,  h_GenMET_px,    "GEN MET p_{x} [GeV]",        "Events", "genmet_px.png",     yoffset=1.01)
-draw_single("c_genmet_py",   -600.0,  600.0,  h_GenMET_py,    "GEN MET p_{y} [GeV]",        "Events", "genmet_py.png",     yoffset=1.01)
-draw_single("c_genmet_sumEt",  0.0,  5000.0,  h_GenMET_sumEt, "GEN MET #SigmaE_{T} [GeV]",  "Events", "genmet_sumEt.png",  yoffset=1.01)
+draw_single("c_genmet_pt",    0.0,  700.0,  h_GenMET_pt,    "GEN MET p_{T} [GeV]",        "Events", "genmet_pt.png",   logy=True,  yoffset=1.01,  ymin_log=0.1)
+draw_single("c_genmet_phi",  -3.5,    3.5,  h_GenMET_phi,   "GEN MET #phi",               "Events", "genmet_phi.png",  logy=True,  yoffset=1.01)
+draw_single("c_genmet_px",  -600.0,  600.0, h_GenMET_px,    "GEN MET p_{x} [GeV]",        "Events", "genmet_px.png",   logy=True,  yoffset=1.01)
+draw_single("c_genmet_py",  -600.0,  600.0, h_GenMET_py,    "GEN MET p_{y} [GeV]",        "Events", "genmet_py.png",   logy=True,  yoffset=1.01)
+draw_single("c_genmet_sumEt", 0.0,  9000.0, h_GenMET_sumEt, "#SigmaE_{T} [GeV]",  "Events", "genmet_sumEt.png", logy=True,  yoffset=1.01)
+
